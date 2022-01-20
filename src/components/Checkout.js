bc609439414bb3a7f721781b7e7790fd36c7e9ea
Emiljano <emiljanodemirxhiu15@gmail.com>
2022-01-20 13:45:21 +0100

    added checkout and basket items

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 import Subtotal from "./Subtotal";
+import { useStateValue } from "../StateProvider";
+import CurrencyFormat from "react-currency-format";
 
 import "./Checkout.css";
 
 function Checkout() {
+  const [{ basket }] = useStateValue();
+
+  console.log(basket);
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -15,6 +21,37 @@ function Checkout() {
         />
         <div>
           <h2 className="checkout__title">Your shopping Basket</h2>
+          {basket.map((item, idx) => {
+            return (
+              <div key={idx} className="checkout__singleItem">
+                <img src={item.image} alt={item.title} />
+                <div className="checkout__description">
+                  <p>{item.title}</p>
+                  <CurrencyFormat
+                    renderText={(value) => (
+                      <strong className="checkout__descriptionPrice">
+                        {`${value}`}
+                      </strong>
+                    )}
+                    decimalScale={2}
+                    value={item.price}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    prefix={"$"}
+                  />
+
+                  <div className="checkout__descriptionRating">
+                    {Array(item.rating)
+                      .fill()
+                      .map((_, i) => {
+                        return <p key={i}>⭐</p>;
+                      })}
+                  </div>
+                  <button>Remove from Basket</button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className="checkout__right">
--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,18 +1,31 @@
 import React from "react";
 
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "../StateProvider";
 
 import "./Subtotal.css";
 
 function Subtotal() {
+  const [{ basket }] = useStateValue();
+
+  const getBasketTotal = (basket) => {
+    let price = 0;
+
+    basket.map((item) => {
+      return (price = price + item.price);
+    });
+
+    return price;
+  };
+
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items):
-              <strong>{/* {`${value}`} */}0</strong>
+              Subtotal ({basket.length} items):
+              <strong> {`${value}`}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -20,8 +33,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        // value={getBasketTotal(basket)}
-        value={2}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
